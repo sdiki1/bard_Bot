fecaad0ebbb23c3bfb86fd95c76524d16dd13c0b
sdiki1 <volodymyrbuharin@yandex.ru>
2023-05-12 22:51:48 +0300

    added logging

--- tg_bot/handlers/echo.py
+++ tg_bot/handlers/echo.py
@@ -2,6 +2,9 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.utils.markdown import hcode
 from tg_bot.BARD import Chatbot
+import logging
+
+logger = logging.getLogger(__name__)
 
 token = 'WggVSk6eVaLHzzDR1DWDARPQHN5NfSWRwEL1kAtB504Q8cAySVa-CF8NUROxEMcQIGVRjw.'
 chatbot = Chatbot(token)
@@ -10,6 +13,7 @@ chatbot = Chatbot(token)
 
 async def bot_echo(message: types.Message):
     answer = chatbot.ask(message.text)
+    logger.info(f'user tg_id: {message.from_user.id}, ask to bard: {message.text}')
     await message.answer(answer['content'])
 
 
--- tg_bot/handlers/start.py
+++ tg_bot/handlers/start.py
@@ -1,9 +1,12 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import CommandStart
+import logging
 
+logger = logging.getLogger(__name__)
 
 async def start(message: types.Message):
-    await message.reply('Hello, this bot will help you with everything using Google Bard, ask the question):')
+    logger.info(f"user tg_ud: {message.from_user.id}, command: /start")
+    await message.reply('Hello, this bot will help you with everything using Google Bard, ask the question, but know, that bard using only english language):')
 
 def register_start(dp: Dispatcher):
     dp.register_message_handler(start, commands='start')
